791bbf3f874211796dafa16541497a83efa1a183
Khanh Nguyen <nguyenhoangkhanh271205@gmail.com>
2025-03-17 22:55:50 +0800

    Read time from RTC module. Display measured temperature.

--- table-clock.ino
+++ table-clock.ino
@@ -1,38 +1,22 @@
-// Include the libraries
+// Include the dependencies
 #include <TM1637Display.h>
-#include <WiFi.h>
-#include <NTPClient.h>
-#include <WiFiUdp.h>
-
-#include "time.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "RTClib.h"
 
 // Define the pins
-#define CLK 23
-#define DIO 22
-#define BTN1 21  // Use for changing display mode
-
-// Define WiFi SSID and password
-const char* WIFI_SSID = "Wokwi-GUEST";
-const char* WIFI_PASSWORD = "";
-
-// Define the timezone
-int timezone = 8;
+#define CLK 32
+#define DIO 25
+#define BTN1 27          // Use to turn into setting value mode
+#define BTN2 14          // Use to edit value
+#define ONE_WIRE_BUS 18  // For temperature sensor
 
 // Create a display object of type TM1637Display
 TM1637Display display = TM1637Display(CLK, DIO);
 
-// Create an array that turns all segments ON
-const uint8_t allON[] = { 0xff, 0xff, 0xff, 0xff };
-
-// Create an array that turns all segments OFF
-const uint8_t allOFF[] = { 0x00, 0x00, 0x00, 0x00 };
-
-// Create an array that sets individual segments per digit to display the word "dOnE"
-const uint8_t done[] = {
-  SEG_B | SEG_C | SEG_D | SEG_E | SEG_G,          // d
-  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,  // O
-  SEG_C | SEG_E | SEG_G,                          // n
-  SEG_A | SEG_D | SEG_E | SEG_F | SEG_G           // E
+// Create letter d (lower-case) symbol
+const uint8_t d_letter[] = {
+  SEG_B | SEG_C | SEG_D | SEG_E | SEG_G  // d
 };
 
 // Create degree celsius symbol
@@ -42,40 +26,30 @@ const uint8_t celsius[] = {
 };
 
 // Store button state
-// BTN1 use pullup => by default is HIGH, when pressed is LOW
+// BTN use pullup => by default is HIGH, when pressed is LOW
 int btn1_prevState = HIGH;
 int btn1_currentState;
+int btn2_prevState = HIGH;
+int btn2_currentState;
+
+// Init a RTC instance
+RTC_DS1307 rtc;
+char daysOfTheWeek[7][12] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature sensor
+DallasTemperature temperature(&oneWire);
 
 // Multiple display modes
-int maxMode = 5; // maximum number of modes
+int maxMode = 5;  // maximum number of modes
 int mode = 0;
 // 0 = HH:MM;
-// 1 = MM:SS;
-// 2 = DD:MM;
+// 1 = _d0x; // Day of the week, d01 = Sunday
+// 2 = DD:MM; // Date and month
 // 3 = YYYY;
 // 4 = DD°C;
-
-// Establish WiFi connection
-void initWiFi() {
-  WiFi.mode(WIFI_STA);  //Set Wi-Fi Mode as station
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
-  Serial.print("Connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(100);
-  }
-
-  Serial.println();
-  Serial.println(WiFi.localIP());
-
-  Serial.print("RSSI: ");
-  Serial.print(WiFi.RSSI());
-  Serial.println(" dBm");
-
-  Serial.print("Connected to WiFi successfully at: ");
-  Serial.println(WIFI_SSID);
-}
 
 void setup() {
   // put your setup code here, to run once:
@@ -84,90 +58,117 @@ void setup() {
 
   // Init the pin config
   pinMode(BTN1, INPUT_PULLUP);  // config BTN1 as input pin and enable the internal pull-up resistor
+  pinMode(BTN2, INPUT_PULLUP);  // config BTN2 as input pin and enable the internal pull-up resistor
 
-  // Init the WiFi connection
-  initWiFi();
+  // Set the brightness (0=dimmest 7=brightest)
+  display.setBrightness(3);
 
-  // Set the brightness to 5 (0=dimmest 7=brightest)
-  display.setBrightness(5);
-
-  // Init and get the time
-  configTime(timezone*3600, 0, "pool.ntp.org");
-}
-
-unsigned long previousMillis = 0;
-const long interval = 1000;  // 1 second
-
-void displayInfo() {
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("Failed to obtain time");
-    return;
+  // Config the RTC
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
   }
 
-  // Get date and time in integers
-  int hour_int = timeinfo.tm_hour;
-  int minute_int = timeinfo.tm_min;
-  int second_int = timeinfo.tm_sec;
+  if (!rtc.isrunning()) {
+    Serial.println("RTC is NOT running, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    //rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
 
-  int day_int = timeinfo.tm_mday;
-  int month_int = timeinfo.tm_mon + 1;
-  int year_int = timeinfo.tm_year + 1900;
-  int weekday_int = timeinfo.tm_wday;
+  // Init the Dallas Temperature
+  temperature.begin();
+}
+
+unsigned long prevRefreshMillis = 0;
+
+void displayInfo() {
+  // Get the current time from the RTC
+  DateTime now = rtc.now();
+
+  // Get date and time in integers
+  int hour_int = now.hour();
+  int minute_int = now.minute();
+  int second_int = now.second();
+
+  int day_int = now.day();
+  int month_int = now.month();
+  int year_int = now.year();
+  int weekday_int = now.dayOfTheWeek() + 1;  // Starting from 1, which is Sunday
 
   // Get the temperature in Celsius
-  int temp_c = 23;
+  temperature.requestTemperatures();
+  float temp_c_float = temperature.getTempCByIndex(0);
+  int temp_c = int(round(temp_c_float));
 
   // Display the integers into the TM1637
   if (mode == 0) {
-    display.showNumberDec(hour_int, true, 2, 0);
-    display.showNumberDecEx(minute_int, 0b11100000, true, 2, 2);
+    display.showNumberDecEx(hour_int * 100 + minute_int, 0b01000000, true, 4, 0);
   } else if (mode == 1) {
-    display.showNumberDec(minute_int, true, 2, 0);
-    display.showNumberDecEx(second_int, 0b11100000, true, 2, 2);
+    display.setSegments(d_letter, 1, 1);
+    display.showNumberDec(weekday_int, true, 2, 2);
   } else if (mode == 2) {
-    display.showNumberDec(day_int, true, 2, 0);
-    display.showNumberDecEx(month_int, 0b11100000, true, 2, 2);
+    display.showNumberDec(day_int * 100 + month_int, true, 4, 0);
   } else if (mode == 3) {
-    display.showNumberDecEx(year_int, 0, true, 4, 0);
+    display.showNumberDec(year_int, true, 4, 0);
   } else if (mode == 4) {
     display.showNumberDec(temp_c, false, 2, 0);
     display.setSegments(celsius, 2, 2);
   }
 
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  // Getting each time field in individual variables
+  // And adding a leading zero when needed;
+  String yearStr = String(now.year(), DEC);
+  String monthStr = (now.month() < 10 ? "0" : "") + String(now.month(), DEC);
+  String dayStr = (now.day() < 10 ? "0" : "") + String(now.day(), DEC);
+  String hourStr = (now.hour() < 10 ? "0" : "") + String(now.hour(), DEC);
+  String minuteStr = (now.minute() < 10 ? "0" : "") + String(now.minute(), DEC);
+  String secondStr = (now.second() < 10 ? "0" : "") + String(now.second(), DEC);
+  String dayOfWeek = daysOfTheWeek[now.dayOfTheWeek()];
+
+  // Complete time string
+  String formattedTime = dayOfWeek + ", " + yearStr + "-" + monthStr + "-" + dayStr + " " + hourStr + ":" + minuteStr + ":" + secondStr;
+
+  // Print the complete formatted time
+  Serial.println(formattedTime);
 }
 
 void loop() {
-  // Attempt to reconnect when WiFi connection drops
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("WiFi lost, reconnecting...");
-    WiFi.disconnect();
-    WiFi.reconnect();
-  }
 
-  // Read BTN1 state
+  // Read BTN1 state // BTN is for turning among setting modes
   btn1_currentState = digitalRead(BTN1);
-
   if (btn1_prevState == HIGH && btn1_currentState == LOW) {
     delay(50);  // Debounce delay
     if (digitalRead(BTN1) == LOW) {
-      Serial.println("The button has been pressed");
-
-      mode += 1;
-      if (mode == maxMode) mode = 0;
-
-      displayInfo();
+      Serial.println("BTN1 has been pressed");
     }
   }
-
   btn1_prevState = btn1_currentState;
 
-  // Check if 1 second has passed
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
+  // Read BTN2 state
+  btn2_currentState = digitalRead(BTN2);
+  if (btn2_prevState == HIGH && btn2_currentState == LOW) {
+    delay(50);  // Debounce delay
+    if (digitalRead(BTN2) == LOW) {
+      Serial.println("BTN2 has been pressed");
+    }
+  }
+  btn2_prevState = btn2_currentState;
 
+  // Check if a period has passed
+  unsigned long currentMillis = millis();
+
+  // Refresh mode after 5 sec
+  if (currentMillis - prevRefreshMillis >= 5000) {
+    prevRefreshMillis = currentMillis;
+    mode += 1;
+    if (mode == maxMode) mode = 0;
+
+    display.clear();
     displayInfo();
   }
 }
@@ -181,3 +182,8 @@ void loop() {
 // [6] https://bytesim.com/blogs/esim/how-to-improve-wifi-signal-strength
 // [7] https://lastminuteengineers.com/esp32-ntp-server-date-time-tutorial/
 // [8] https://www.geeksforgeeks.org/time-h-header-file-in-c-with-examples/
+// [9] https://www.upesy.com/blogs/tutorials/how-to-use-gpio-pins-of-esp32-with-arduino?srsltid=AfmBOorA66QR6KnZLyeLmbsXRWgIx_rGInB9UIDc2FU-AX1Tg714yGDv
+// [10] https://randomnerdtutorials.com/guide-for-ds18b20-temperature-sensor-with-arduino/
+// [11] https://randomnerdtutorials.com/esp32-ds1307-real-time-clock-rtc-arduino/
+// [12] https://randomnerdtutorials.com/esp32-flash-memory/
+// [13] https://randomnerdtutorials.com/esp32-pinout-reference-gpios/
